3a4d167dc948b7d9d7ff4f83ac8cebf34181bbd8
Shubhankar Banerjee <shubhankarbanerjee16@gmail.com>
2023-02-26 09:49:12 +0530

    Added JS for lesson 2.

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ for (let c = 0; c <= 12; c++) {
     checkForLs1();
   });
 }
+
+// Click functions for Lessons 2.
+function checkForLs2() {
+  for (let d = 13; d <= 28; d++) {
+    if (check[d].checked) {
+      tr[d + 2].classList.add("strike-through");
+    } else {
+      tr[d + 2].classList.remove("strike-through");
+    }
+  }
+}
+
+for (let e = 15; e <= 30; e++) {
+  tr[e].addEventListener("click", function strikeAndCheck() {
+    check[e - 2].click();
+    checkForLs2();
+  });
+}
+
+for (let f = 0; f <= 28; f++) {
+  check[f].addEventListener("click", () => {
+    check[f].click();
+    checkForLs2();
+  });
+}
